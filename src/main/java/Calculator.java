226d09545a64e4f468401b8f0268c592f99de293
St1gt <St1gt@yandex.ru>
2023-12-17 20:23:17 +0300

    Add new realisation

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,46 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        switch (b) {
+            case 0:
+                throw new ArithmeticException("На ноль делить нельзя!!!");
+            default:
+                return a / b;
+        }
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int result = 0;
+        int operand1 = 5;
+        int operand2 = 3;
+        char operation = '+';
+
+        switch (operation) {
+            case '+':
+                result = add(operand1, operand2);
+                break;
+            case '-':
+                result = dif(operand1, operand2);
+                break;
+            case '*':
+                result = times(operand1, operand2);
+                break;
+            case '/':
+                result = div(operand1, operand2);
+                break;
+        }
+
+        return result;
     }
 }
